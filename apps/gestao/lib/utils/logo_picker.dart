9563e363b11993befa49d205d89eccf0faf91630
sapaodev <32149187+sapodexterdev@users.noreply.github.com>
2026-07-29 15:14:05 -0300

    Adiciona perfis de otimização de imagens

--- apps/gestao/lib/utils/logo_picker.dart
+++ apps/gestao/lib/utils/logo_picker.dart
@@ -2,4 +2,17 @@ import 'logo_picker_stub.dart' if (dart.library.html) 'logo_picker_web.dart'
     as picker;
 import 'logo_file.dart';
 
-Future<LogoFile?> pickLogoFile() => picker.pickLogoFile();
+Future<LogoFile?> pickLogoFile({
+  required int maxWidth,
+  required int maxHeight,
+  required int compressionThresholdBytes,
+  double quality = 0.86,
+  bool preserveTransparency = false,
+}) =>
+    picker.pickLogoFile(
+      maxWidth: maxWidth,
+      maxHeight: maxHeight,
+      compressionThresholdBytes: compressionThresholdBytes,
+      quality: quality,
+      preserveTransparency: preserveTransparency,
+    );
