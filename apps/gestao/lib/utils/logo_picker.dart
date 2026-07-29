be2f34801d01b26b6373dfd83bea74e432068c1a
sapaodev <32149187+sapodexterdev@users.noreply.github.com>
2026-07-29 15:21:07 -0300

    Estabiliza parâmetros da otimização de imagens

--- apps/gestao/lib/utils/logo_picker.dart
+++ apps/gestao/lib/utils/logo_picker.dart
@@ -3,9 +3,9 @@ import 'logo_picker_stub.dart' if (dart.library.html) 'logo_picker_web.dart'
 import 'logo_file.dart';
 
 Future<LogoFile?> pickLogoFile({
-  required int maxWidth,
-  required int maxHeight,
-  required int compressionThresholdBytes,
+  int maxWidth = 1920,
+  int maxHeight = 1080,
+  int compressionThresholdBytes = 900 * 1024,
   double quality = 0.86,
   bool preserveTransparency = false,
 }) =>
